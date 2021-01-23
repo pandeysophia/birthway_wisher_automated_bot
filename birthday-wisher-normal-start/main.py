f9e70322ded1f74d140618d959011e62f6710a03
pandeysophia <sophiapandey2017@gmail.com>
2021-01-22 18:08:10 -0700

    Initial commit

---
+++ birthday-wisher-normal-start/main.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import pandas
+import random
+import smtplib
+PORT = 587
+
+my_email = "avanepali@gmail.com"
+password = "Anepali1!"
+
+today = (datetime.now().month, datetime.now().day)
+
+data = pandas.read_csv("birthdays.csv")
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today in birthday_dict:
+    birthday_person = birthday_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com", PORT) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday\n\n {contents}")
+
+
